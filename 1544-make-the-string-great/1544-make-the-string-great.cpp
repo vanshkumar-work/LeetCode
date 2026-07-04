f21f9fa850bb3e644e796d185d19061dafd2639d
Vansh Kumar <vanshkumar.work.ex@gmail.com>
2026-07-05 01:45:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (25.12%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st ;
+        for (char ch : s){
+            if(!st.empty()&&abs(ch-st.top())==32){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        
+        string ans ;
+        while(!st.empty()){
+            ans+=st.top() ;
+            st.pop() ;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans ;
+        
+    }
+};
