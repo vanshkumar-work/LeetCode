108b463e92232f806f77eafff833839fabe8a2e4
Vansh Kumar <vanshkumar.work.ex@gmail.com>
2026-07-13 02:18:52 +0530

    Time: 0 ms (100%), Space: 9.2 MB (78.52%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin() , t.end());
+        char ans ;
+        for(int i = 0  ; i<s.length() ; i++){
+            if(s[i]!=t[i]){
+                ans = t[i];
+                break ;
+            }
+        }
+        return ans ;
+    }
+};
