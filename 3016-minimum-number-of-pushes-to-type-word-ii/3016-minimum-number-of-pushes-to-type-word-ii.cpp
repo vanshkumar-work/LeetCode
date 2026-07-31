26393a5af893d6c607cbea1d72867d6a2a1e148e
Vansh Kumar <vanshkumar.work.ex@gmail.com>
2026-07-31 16:04:28 +0530

    Time: 6 ms (75.61%), Space: 28.4 MB (25.39%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26,0) ;
+        for(char c : word){
+            freq[c-'a']++ ;
+        }
+        sort(freq.rbegin(),freq.rend());
+        int ans = 0 ;
+        for(int i = 0 ; i<26 ; i++){
+            ans = ans + (freq[i] * ((i/8)+1)) ;
+            if(freq[i]==0) break ;
+        }
+        return ans ;
+        
+    }
+};
