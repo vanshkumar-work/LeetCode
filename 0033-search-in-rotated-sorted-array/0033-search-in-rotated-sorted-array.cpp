1bb532294c413745a2a5c70970ca256579369c93
Vansh Kumar <vanshkumar.work.ex@gmail.com>
2026-08-05 03:12:05 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.11%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,47 +1,29 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
-        int left = 0;
-        int right = nums.size() - 1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            // target found
-            if (nums[mid] == target)
-                return mid;
-
-            // LEFT HALF SORTED
-            if (nums[left] <= nums[mid]) {
-
-                // target lies inside left half
-                if (target >= nums[left] &&
-                    target < nums[mid]) {
-
-                    right = mid - 1;
+        int left = 0 ;
+        int right = nums.size() - 1 ;
+        int ans = -1 ;
+        while(left<=right){
+            int mid = left + (right - left)/2 ;
+            if(nums[mid]==target) {
+                ans = mid ;
+                break ;
+            }
+            else if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && nums[mid]>target){
+                    right = mid -1 ;
                 }
                 else {
-                    left = mid + 1;
+                    left = mid + 1 ;
                 }
             }
-
-            // RIGHT HALF SORTED
-            else {
-
-                // target lies inside right half
-                if (target > nums[mid] &&
-                    target <= nums[right]) {
-
-                    left = mid + 1;
+            else{
+                if(nums[right]>=target && nums[mid]<target){
+                    left = mid+1 ;
                 }
-                else {
-                    right = mid - 1;
-                }
-            }
-        }
-
-        return -1;
+                else right =  mid -1 ;            }
+            } 
+        return ans; 
     }
 };
